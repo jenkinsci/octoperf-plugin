2e34434bac3c9df3aa3b09d4385640dfc658c88b
jloisel <loisel.jerome@gmail.com>
2019-02-21 10:57:38 +0100

    fix junit

--- src/main/java/org/jenkinsci/plugins/octoperf/client/RestClientService.java
+++ src/main/java/org/jenkinsci/plugins/octoperf/client/RestClientService.java
@@ -17,7 +17,7 @@ public interface RestClientService {
   /**
    * Singleton {@link RestClientService} instance.
    */
-  RestClientService CLIENTS = new RetrofitClientService(Jenkins.get());
+  RestClientService CLIENTS = new RetrofitClientService(Jenkins.getInstanceOrNull());
 
   /**
    * Creates a {@link Retrofit} ready to login on octoperf cloud load testing platform.
