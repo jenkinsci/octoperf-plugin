8e6a4562d91236550dfacd36d3e8186f98149bdd
jloisel <loisel.jerome@gmail.com>
2019-11-13 11:43:21 +0100

    update dependencies

--- src/main/java/org/jenkinsci/plugins/octoperf/result/BenchResult.java
+++ src/main/java/org/jenkinsci/plugins/octoperf/result/BenchResult.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
-import lombok.experimental.Wither;
+import lombok.With;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -14,7 +14,7 @@ public class BenchResult {
   String id;
   String designProjectId;
   String resultProjectId;
-  @Wither
+  @With
   BenchResultState state;
 
   @JsonCreator
